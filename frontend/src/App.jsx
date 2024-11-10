4aff9e98910c7bf3ca7e798eeadf87119956c8a5
Jonathan Muhire <jonathanmuhire@Jonathans-MacBook-Pro-3.local>
2024-11-10 00:20:29 -0600

    added App frontend display

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// frontend/src/App.jsx
+import React from 'react';
+import SensorDashboard from './components/SensorDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <SensorDashboard />
+    </div>
+  );
+}
+
+export default App;
